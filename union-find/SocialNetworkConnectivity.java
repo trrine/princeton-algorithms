08ad52a4943cfbad89de4fd896ce0ca3a5dd2fea
Trine <41973043+trrine@users.noreply.github.com>
2023-03-12 23:17:53 +1100

    Update SocialNetworkConnectivity.java

--- union-find/SocialNetworkConnectivity.java
+++ union-find/SocialNetworkConnectivity.java
@@ -1,1 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class SocialNetworkConnectivity {
+    public static void main(String[] args) {
+        String date, time;
+        int n = 5; // number of members
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+
+        try {
+            File logFile = new File("friendships.txt");
+            Scanner scanner = new Scanner(logFile);
+
+            while (scanner.hasNextLine()) {
+                int memberA = scanner.nextInt();
+                int memberB = scanner.nextInt();
+                date = scanner.next();
+                time = scanner.next();
+
+                // form friendship
+                uf.union(memberA, memberB);
+
+                if (uf.count() == 1) {
+                    System.out.println("All members connected at timestamp: " + date + " " + time);
+                    break;
+                }
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+}
